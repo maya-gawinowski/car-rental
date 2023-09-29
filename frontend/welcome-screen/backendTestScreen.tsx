386bc16c945c69ca3aa05d56a42a9b194dfffbb9
Michael <michael@scnyder.cc>
2023-09-29 13:28:46 +0200

    Fix host so that it works for iOS as well

--- frontend/welcome-screen/backendTestScreen.tsx
+++ frontend/welcome-screen/backendTestScreen.tsx
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { View, Text, FlatList, Platform } from 'react-native'
+import axios from 'axios'
 
 const CarsList = () => {
-    type Car = {
-        id: number;
-        brand: string;
-        model: string;
-      };
-      
-      const [cars, setCars] = useState<Car[]>([]);
-      
+  type Car = {
+    id: number
+    brand: string
+    model: string
+  }
+
+  const [cars, setCars] = useState<Car[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://10.0.2.2:5000/cars');
-        setCars(response.data);
+        const host = Platform.OS === 'ios' ? 'localhost' : '10.0.2.2'
+        const url = `http://${host}:3000/cars`
+        const response = await axios.get(url)
+        setCars(response.data)
       } catch (error) {
-        console.error('There was an error fetching data', error);
+        console.error('There was an error fetching data', error)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
     <View>
@@ -31,11 +32,13 @@ const CarsList = () => {
         data={cars}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <Text>{item.brand} - {item.model}</Text>
+          <Text>
+            {item.brand} - {item.model}
+          </Text>
         )}
       />
     </View>
-  );
-};
+  )
+}
 
-export default CarsList;
+export default CarsList
